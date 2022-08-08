b51e0f5ee93f53e61e966d91d578f102dd6ddcf1
Ibrahim Dauda <dabdulxyz@gmail.com>
2022-08-08 11:49:28 +0100

    Profile Pic Field added to DB

--- controllers/userController.js
+++ controllers/userController.js
@@ -42,6 +42,7 @@ const registerUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       phoneNumber: user.phoneNumber,
+      profileImg: user.profileImg,
       userRole: user.userRole,
       userStatus: user.userStatus,
       token: generateToken(user._id),

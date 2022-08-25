a364bf83cbfa08fd60fb4b5a38a1b75768f428f6
Ibrahim Dauda <dabdulxyz@gmail.com>
2022-08-25 10:06:15 -0700

    Cors Added From Backend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
+const cors = require("cors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT;
@@ -9,6 +10,7 @@ connectDB();
 const app = express();
 
 // Middlewares
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 

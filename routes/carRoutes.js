0b9c385ed7408088e113a9a95c14e3397421f378
Ibrahim Dauda <dabdulxyz@gmail.com>
2022-09-30 10:48:58 +0100

    Get Car By ID CONTROLLER & ROUTE

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -11,10 +11,10 @@ const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.route("/add-car-details").post(protect, addCarDetails);
-router.route("/:id").get(protect, getCarById);
 router.route("/cars/:ride_city").get(protect, getCarsByLocation);
 router.route("/available-cars").get(protect, getCarsByAvailablity);
 router.route("/booking").post(protect, carBooking);
 router.route("/bookings/user/:id").get(protect, getUserBookings);
+router.route("/car/:id").get(protect, getCarById);
 
 module.exports = router;

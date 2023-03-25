19d2f2ad763748b8f6f6a479d0ca332c8f327e58
dabdu <dabdulxyz@gmail.com>
2023-03-25 16:39:16 +0100

    Notification Model COntroller, Model & Routes Created

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -6,5 +6,15 @@ const getNotificationsByUserId = asyncHandler(async (req, res) => {
   });
   res.status(200).json(notifications);
 });
+const onReadNotification = asyncHandler(async (req, res) => {
+  const read = await Notification.findByIdAndUpdate(
+    req.params.notificationId,
+    { status: "read" },
+    {
+      new: true,
+    }
+  );
+  res.status(201).send(read);
+});
 
-module.exports = { getNotificationsByUserId };
+module.exports = { getNotificationsByUserId, onReadNotification };
--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {
   getNotificationsByUserId,
+  onReadNotification,
 } = require("../controllers/notificationController");
 const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.route("/:userId").get(protect, getNotificationsByUserId);
+router.route("/:notificationId").put(protect, onReadNotification);
 
 module.exports = router;

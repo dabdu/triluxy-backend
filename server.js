2af7a949be7534376e2498a8791351cff7645a1b
dabdu <dabdulxyz@gmail.com>
2023-08-25 06:34:59 +0100

    Cors Resolved

--- server.js
+++ server.js
@@ -12,10 +12,17 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://tribackoffice.vercel.app"],
     credentials: true,
   })
 );
+// Middleware to set up CORS headers
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://tribackoffice.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 

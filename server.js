2a668b78699c299b83aded980dd038647094666d
dabdu <dabdulxyz@gmail.com>
2023-08-26 18:21:39 +0100

    Get SIngle Order Details

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(
 // Middleware to set up CORS headers
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://tribackoffice.vercel.app");
-  // res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Add this line
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Add this line
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
